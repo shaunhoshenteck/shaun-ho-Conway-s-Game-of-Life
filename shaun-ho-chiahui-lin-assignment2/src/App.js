bf032c17410488a3f9afa1f16a675e4648491cb2
shaunhoshenteck <ho.s@husky.neu.edu>
2020-11-09 16:21:38 -0800

    Worked on landing page

--- shaun-ho-chiahui-lin-assignment2/src/App.js
+++ shaun-ho-chiahui-lin-assignment2/src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
-import React from "react";
-import { Main } from "./Main";
-import Homepage from "./components/Hompage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+import React from 'react';
+import { Main } from './Main';
+import Homepage from './components/Hompage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/game">
-          <Main />
-        </Route>
-        <Route path="/">
-          <Homepage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/game">
+                    <Main />
+                </Route>
+                <Route path="/">
+                    <Homepage />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
